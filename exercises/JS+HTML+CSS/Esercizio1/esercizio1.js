dabe1dd02fbad11c11ab3ee0fdb013dada7e0fb2
ValerioErnestoColibro <valerio.colibro@gmail.com>
2024-05-09 13:59:41 +0200

    feat(valerio_colibro:trying to create an app

--- exercises/JS+HTML+CSS/Esercizio1/esercizio1.js
+++ exercises/JS+HTML+CSS/Esercizio1/esercizio1.js
@@ -1,5 +1,5 @@
 function cambiaColore() {
   // il testo bianco è il nome della funzione, poi nelle graffe c'è il contenuto della funzione //
-  var paragrafo = document.querySelector("p"); //
+  var paragrafo = document.querySelector("p"); //Prendi una variabile e selezionala tramite un selector di documenti, non scordare il parametro//
   paragrafo.style.color = "red"; // Imposta il colore del testo del paragrafo su rosso //
 }
